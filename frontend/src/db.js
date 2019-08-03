62e9415d99fb3905faae9081ea8b05cde3ef4787
Philip Johansson <phi@qgr.se>
2019-08-03 18:06:40 +0200

    Away with ze frontend

--- frontend/src/db.js
+++
@@ -1,7 +0,0 @@
-import firebase from 'firebase'
-
-export const db = firebase
-  .initializeApp({ projectId: 'bostadskoe' })
-  .firestore()
-
-export const { TimeStamp, GeoPoint } = firebase.firestore
